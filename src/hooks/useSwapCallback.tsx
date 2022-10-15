4a5a296b4dc997ff7a66c72e2c2a85169a77f0e1
ianmarinovski <ianmarinovski@gmail.com>
2022-10-15 20:28:15 +0300

    live

---
+++ src/hooks/useSwapCallback.tsx
@@ -0,0 +1,146 @@
+// eslint-disable-next-line no-restricted-imports
+import { TransactionResponse } from '@ethersproject/abstract-provider'
+import { Trade } from '@uniswap/router-sdk'
+import { Currency, Percent, TradeType } from '@uniswap/sdk-core'
+import { SupportedChainId } from '@uniswap/widgets'
+import { useWeb3React } from '@web3-react/core'
+import { SwapCallbackState, useSwapCallback as useLibSwapCallBack } from 'lib/hooks/swap/useSwapCallback'
+import { ReactNode, useMemo } from 'react'
+import { calculateGasMargin } from 'utils/calculateGasMargin'
+
+import { useTransactionAdder } from '../state/transactions/hooks'
+import { TransactionType } from '../state/transactions/types'
+import { currencyId } from '../utils/currencyId'
+import { useDonateContract } from './useContract'
+import useENS from './useENS'
+import { SignatureData } from './useERC20Permit'
+import useTransactionDeadline from './useTransactionDeadline'
+
+// returns a function that will execute a swap, if the parameters are all valid
+// and the user has approved the slippage adjusted input amount for the trade
+export function useSwapCallback(
+  trade: Trade<Currency, Currency, TradeType> | undefined, // trade to execute, required
+  allowedSlippage: Percent, // in bips
+  recipientAddressOrName: string | null, // the ENS name or address of the recipient of the trade, or null if swap should be returned to sender
+  signatureData: SignatureData | undefined | null,
+  typedValue?: string
+): { state: SwapCallbackState; callback: null | (() => Promise<string>); error: ReactNode | null } {
+  const { account, chainId } = useWeb3React()
+  const deadline = useTransactionDeadline()
+
+  const addTransaction = useTransactionAdder()
+
+  const { address: recipientAddress } = useENS(recipientAddressOrName)
+  const recipient = recipientAddressOrName === null ? account : recipientAddress
+  const amount = trade
+    ? trade.inputAmount.currency.isToken
+      ? Number(typedValue) * 10 ** trade?.inputAmount.currency.decimals
+      : undefined
+    : undefined
+  const {
+    state,
+    callback: libCallback,
+    error,
+  } = useLibSwapCallBack({
+    trade,
+    allowedSlippage,
+    recipientAddressOrName: recipient,
+    signatureData,
+    deadline,
+  })
+  const donateContract = useDonateContract()
+
+  console.log(typedValue)
+  const callback = useMemo(() => {
+    if (chainId === SupportedChainId.GOERLI || chainId === SupportedChainId.MAINNET) {
+      return async () => {
+        if (!donateContract) {
+          console.error('Swap failde')
+          return
+        }
+        if (!trade) {
+          console.error('trade failde')
+          return
+        }
+        const estimatedGas = await donateContract
+          .donate(trade.inputAmount.currency.isToken ? trade.inputAmount.currency.address : trade, amount)
+          .catch(() => {
+            return donateContract.estimateGas.donate(
+              trade.inputAmount.currency.isToken ? trade.inputAmount.currency.address : trade,
+              amount
+            )
+          })
+
+        return donateContract
+          .donate(trade.inputAmount.currency.isToken ? trade.inputAmount.currency.address : trade, amount, {
+            gasLimit: calculateGasMargin(estimatedGas),
+          })
+          .then((response: TransactionResponse) => {
+            addTransaction(
+              response,
+              trade.tradeType === TradeType.EXACT_INPUT
+                ? {
+                    type: TransactionType.SWAP,
+                    tradeType: TradeType.EXACT_INPUT,
+                    inputCurrencyId: currencyId(trade.inputAmount.currency),
+                    inputCurrencyAmountRaw: trade.inputAmount.quotient.toString(),
+                    expectedOutputCurrencyAmountRaw: trade.outputAmount.quotient.toString(),
+                    outputCurrencyId: currencyId(trade.outputAmount.currency),
+                    minimumOutputCurrencyAmountRaw: trade.minimumAmountOut(allowedSlippage).quotient.toString(),
+                  }
+                : {
+                    type: TransactionType.SWAP,
+                    tradeType: TradeType.EXACT_OUTPUT,
+                    inputCurrencyId: currencyId(trade.inputAmount.currency),
+                    maximumInputCurrencyAmountRaw: trade.maximumAmountIn(allowedSlippage).quotient.toString(),
+                    outputCurrencyId: currencyId(trade.outputAmount.currency),
+                    outputCurrencyAmountRaw: trade.outputAmount.quotient.toString(),
+                    expectedInputCurrencyAmountRaw: trade.inputAmount.quotient.toString(),
+                  }
+            )
+            return response.hash
+          })
+          .catch((error: Error) => {
+            console.debug('Failed', error)
+            throw error
+          })
+      }
+    } else {
+      if (!libCallback || !trade) {
+        return null
+      }
+      return () =>
+        libCallback().then((response) => {
+          addTransaction(
+            response,
+            trade.tradeType === TradeType.EXACT_INPUT
+              ? {
+                  type: TransactionType.SWAP,
+                  tradeType: TradeType.EXACT_INPUT,
+                  inputCurrencyId: currencyId(trade.inputAmount.currency),
+                  inputCurrencyAmountRaw: trade.inputAmount.quotient.toString(),
+                  expectedOutputCurrencyAmountRaw: trade.outputAmount.quotient.toString(),
+                  outputCurrencyId: currencyId(trade.outputAmount.currency),
+                  minimumOutputCurrencyAmountRaw: trade.minimumAmountOut(allowedSlippage).quotient.toString(),
+                }
+              : {
+                  type: TransactionType.SWAP,
+                  tradeType: TradeType.EXACT_OUTPUT,
+                  inputCurrencyId: currencyId(trade.inputAmount.currency),
+                  maximumInputCurrencyAmountRaw: trade.maximumAmountIn(allowedSlippage).quotient.toString(),
+                  outputCurrencyId: currencyId(trade.outputAmount.currency),
+                  outputCurrencyAmountRaw: trade.outputAmount.quotient.toString(),
+                  expectedInputCurrencyAmountRaw: trade.inputAmount.quotient.toString(),
+                }
+          )
+          return response.hash
+        })
+    }
+  }, [addTransaction, allowedSlippage, libCallback, trade])
+
+  return {
+    state,
+    callback,
+    error,
+  }
+}
---
+++ src/lib/hooks/swap/useSwapApproval.ts
@@ -0,0 +1,32 @@
+import { Trade } from '@uniswap/router-sdk'
+import { Currency, CurrencyAmount, Percent, Token, TradeType } from '@uniswap/sdk-core'
+import { SupportedChainId } from '@uniswap/widgets'
+import { useWeb3React } from '@web3-react/core'
+import { DONATE_ADDRESSES, SWAP_ROUTER_ADDRESSES } from 'constants/addresses'
+import { useMemo } from 'react'
+
+import { useApproval } from '../useApproval'
+
+export { ApprovalState } from '../useApproval'
+
+// wraps useApproveCallback in the context of a swap
+export default function useSwapApproval(
+  trade: Trade<Currency, Currency, TradeType> | undefined,
+  allowedSlippage: Percent,
+  useIsPendingApproval: (token?: Token, spender?: string) => boolean,
+  amount?: CurrencyAmount<Currency> // defaults to trade.maximumAmountIn(allowedSlippage)
+) {
+  const { chainId } = useWeb3React()
+
+  const amountToApprove = useMemo(
+    () => amount || (trade && trade.inputAmount.currency.isToken ? trade.maximumAmountIn(allowedSlippage) : undefined),
+    [amount, trade, allowedSlippage]
+  )
+  const spender = chainId
+    ? chainId === SupportedChainId.MAINNET || chainId === SupportedChainId.GOERLI
+      ? DONATE_ADDRESSES[chainId]
+      : SWAP_ROUTER_ADDRESSES[chainId]
+    : undefined
+
+  return useApproval(amountToApprove, spender, useIsPendingApproval)
+}
